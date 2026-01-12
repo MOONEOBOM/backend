0eff92a56d9733aa1192c8b8e0e103c27dfc1aa0
tree0-0 <gkdlrne97@naver.com>
2026-01-13 00:56:32 +0900

    feat: 유저정보보기 기능 추가

--- src/main/java/com/ureca/ureca/domain/user/controller/UserController.java
+++ src/main/java/com/ureca/ureca/domain/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.ureca.ureca.domain.user.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import com.ureca.ureca.domain.user.dto.User;
 import com.ureca.ureca.domain.user.service.UserService;
 import com.ureca.ureca.global.common.response.ApiResponse;
@@ -15,9 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
   private final UserService userService;
 
-  @GetMapping("/{id}")
-  public ApiResponse<User> get(@PathVariable("id") Long id) {
-    User data = userService.getUser(id);
-    return ApiResponse.ok(data);
+  @GetMapping("/me")
+  public ApiResponse<User> me(Authentication authentication) {
+    String firebaseUid = (String) authentication.getPrincipal(); // 필터에서 uid 넣어둠
+    User user = userService.getByFirebaseUid(firebaseUid);
+    
+    return ApiResponse.ok("유저 조회 성공",user);
   }
 }
