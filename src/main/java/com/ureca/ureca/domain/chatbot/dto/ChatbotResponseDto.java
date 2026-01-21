c9df7bba2946bbfa54c80fcdcb44230a71ec7354
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-01-21 15:39:24 +0900

    Fix: 오탈자 수정

--- src/main/java/com/ureca/ureca/domain/chatbot/dto/ChatbotResponseDto.java
+++ src/main/java/com/ureca/ureca/domain/chatbot/dto/ChatbotResponseDto.java
@@ -7,10 +7,10 @@ import lombok.Data;
 
 @Data
 public class ChatbotResponseDto {
-	private List<ScenarioDetail> answer;
+	private List<ChatbotDetail> answer;
 	
 	@Data
-    public static class ScenarioDetail {
+    public static class ChatbotDetail {
         private String normal;    // 일반적인 답변
         private String easy; // 쉬운 답변
     }
