30260b7117cc355fff1cfec81264afe6dea8fd91
RyuJaeHwi <yujaehwi02@gmail.com>
2026-01-22 15:41:27 +0900

    fix: 답변 dto 축소, 프롬프트 수정

--- src/main/java/com/ureca/ureca/domain/summary/dto/SummaryResponseDto.java
+++ src/main/java/com/ureca/ureca/domain/summary/dto/SummaryResponseDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 public class SummaryResponseDto {
+	
     private String title;  // 요약본 제목 (카드 타이틀)
     private String summary;  // 상담 요약본 (3 ~ 4줄)
     @JsonProperty("core_chat")
@@ -17,17 +18,12 @@ public class SummaryResponseDto {
 
     @Data
     public static class CoreChat {
-    	@JsonProperty("counselor")
-    	private ChatMessage counselor;
-    	@JsonProperty("customer")
-    	private ChatMessage customer;   
-    }
-    
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class ChatMessage {
-        private String speaker;
-        private String text;
+        // 상담사 핵심 정보
+        private String counselor;        // "상담사"
+        private String counselormessage; // 상담사 핵심 대화 내용
+        
+        // 고객 핵심 정보
+        private String customer;         // "고객"
+        private String customermessage;  // 고객 핵심 대화 내용
     }
 }
